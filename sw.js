83a711cc27da8b7a3accf9dbc9fecf165269500f
djwandrei <djwandrei@gmail.com>
2026-05-26 19:40:43 -0500

    Fix stale archive links after debug scan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dj-house-v2026-05-26-05';
+const CACHE_VERSION = 'dj-house-v2026-05-26-06';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const CATALOG_CACHE = `${CACHE_VERSION}-catalog`;
